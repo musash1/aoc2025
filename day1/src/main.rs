a8cbb57356f9f072bf4493d2b1e6014be42a3d08
musash1 <luis.gjokaj@proton.me>
2025-12-03 13:24:37 +0100

    day1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -19,23 +19,21 @@ fn part1(lines: &str) -> i32 {
             } else if start - instruction.1 < 0 {
                 start = 100 - (start - instruction.1).abs();
             } else {
-                start = start - instruction.1;
+                start -= instruction.1;
             }
+        } else if (start + instruction.1) % 100 == 0 {
+            count += 1;
+            start = 0;
+        } else if start + instruction.1 > 100 {
+            start = start + instruction.1 - 100;
         } else {
-            if (start + instruction.1) % 100 == 0 {
-                count += 1;
-                start = 0;
-            } else if start + instruction.1 > 100 {
-                start = start + instruction.1 - 100;
-            } else {
-                start = start + instruction.1;
-            }
+            start += instruction.1;
         }
     }
     count
 }
 
-fn part2(lines: &str) -> usize {
+fn part2(lines: &str) -> i32 {
     let mut start = 50;
     let mut count = 0;
     let instructions: Vec<(&str, i32)> = lines
@@ -47,33 +45,25 @@ fn part2(lines: &str) -> usize {
         .collect();
 
     for instruction in instructions {
+        let mut num = instruction.1;
         if instruction.0 == "L" {
-            if (start - instruction.1) % 100 == 0 {
-                count += 1;
-                start = 0;
-            } else if start - instruction.1 < 0 {
-                count += 1;
-                start = 100 - (start - instruction.1).abs();
-            } else {
-                start = start - instruction.1;
-            }
-        } else {
-            if (start + instruction.1) % 100 == 0 {
-                count += 1;
-                start = 0;
-            } else if start + instruction.1 > 100 {
-                count += 1;
-                start = start + instruction.1 - 100;
-            } else {
-                start = start + instruction.1;
-            }
+            num = -num;
+        }
+        if start == 0 && num < 0 {
+            count -= 1;
+        }
+        start += num;
+        count += start.div_euclid(100).abs();
+        start = start.rem_euclid(100);
+        if start == 0 && num < 0 {
+            count += 1
         }
     }
     count
 }
 
 fn main() {
-    let lines: String = read_to_string("src/test.txt").unwrap();
+    let lines: String = read_to_string("src/day1.txt").unwrap();
     let part1 = part1(&lines);
     let part2 = part2(&lines);
 
