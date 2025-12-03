9809db18540abfec920e56f7be5f06a357e7942f
musash1 <luis.gjokaj@proton.me>
2025-12-03 08:17:58 +0100

    day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -36,11 +36,44 @@ fn part1(lines: &str) -> i32 {
 }
 
 fn part2(lines: &str) -> usize {
-    0
+    let mut start = 50;
+    let mut count = 0;
+    let instructions: Vec<(&str, i32)> = lines
+        .lines()
+        .map(|l| {
+            let (direction, num) = l.split_at(1);
+            (direction, num.parse::<i32>().unwrap())
+        })
+        .collect();
+
+    for instruction in instructions {
+        if instruction.0 == "L" {
+            if (start - instruction.1) % 100 == 0 {
+                count += 1;
+                start = 0;
+            } else if start - instruction.1 < 0 {
+                count += 1;
+                start = 100 - (start - instruction.1).abs();
+            } else {
+                start = start - instruction.1;
+            }
+        } else {
+            if (start + instruction.1) % 100 == 0 {
+                count += 1;
+                start = 0;
+            } else if start + instruction.1 > 100 {
+                count += 1;
+                start = start + instruction.1 - 100;
+            } else {
+                start = start + instruction.1;
+            }
+        }
+    }
+    count
 }
 
 fn main() {
-    let lines: String = read_to_string("src/day1.txt").unwrap();
+    let lines: String = read_to_string("src/test.txt").unwrap();
     let part1 = part1(&lines);
     let part2 = part2(&lines);
 
